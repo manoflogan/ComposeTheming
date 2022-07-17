c758200f0889ddcc296eb5106e12a7aab5eed1d3
manoflogan <manoflogan@gmail.com>
2022-07-16 23:48:43 -0700

    [Lesson 4] Define the light theme, typography, colours, and shapes.

---
+++ app/src/main/java/com/codelab/theming/ui/start/Themes.kt
@@ -0,0 +1,32 @@
+package com.codelab.theming.ui.start
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun JetnewsTheme(isDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable ()-> Unit) {
+    MaterialTheme(
+        content = content,
+        colors = if (isDarkTheme) DarkColours else LightColours,
+        typography = JetnewsTypography,
+        shapes = JetNewsShapes
+    )
+}
+
+private val LightColours = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    error = Red800,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black,
+    onError = Color.White
+)
+
+private val DarkColours = darkColors()
---
+++ app/src/main/java/com/codelab/theming/ui/start/Typography.kt
@@ -0,0 +1,74 @@
+package com.codelab.theming.ui.start
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.codelab.theming.R
+
+private val MontSerrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_medium, FontWeight.W500),
+    Font(R.font.montserrat_semibold, FontWeight.W600)
+)
+
+private val Domine = FontFamily(
+    Font(R.font.domine_regular),
+    Font(R.font.domine_bold)
+)
+
+val JetnewsTypography = Typography(
+    h4 = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W600,
+        fontSize = 30.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Domine,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = MontSerrat,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp
+    )
+
+)
